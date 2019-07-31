c48423dd2faa2002cdce71e59faeb4f1526809e8
kcars409 <32416704+kcars409@users.noreply.github.com>
2019-07-31 17:40:35 -0500

    Update image path in appsettings.json; Add Dropzone files.

---
+++ JennySeeversPhotography/Controllers/UploadController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace JennySeeversPhotography.Controllers
+{
+    [Route("admin")]
+    public class UploadController : Controller
+    {
+        private readonly IConfiguration _config;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UploadController(IConfiguration c, UserManager<IdentityUser> um)
+        {
+            _userManager = um;
+            _config = c;
+        }
+
+        [HttpGet("/media/{projID}/{picID}")]
+        public async Task<IActionResult> GetPic(int projID, int picID)
+        {
+            var filePath = _config.GetValue<string>("UploadPath") + projID + "/" + picID;
+            if (System.IO.File.Exists(filePath))
+            {
+                var imageFile = System.IO.File.OpenRead(filePath);
+                return File(imageFile, "image/jpg");
+            } else
+            {
+                return NotFound();
+            }
+        }
+
+        //[Authorize(Roles = _userManager.Users.)]
+        [HttpPost("/upload")]
+        public async Task<IActionResult> UploadPic(IFormFile file)
+        {
+            var filePath = _config.GetValue<string>("UploadPath");
+
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            var fullFile = filePath + file.FileName;
+
+            using (var stream = new FileStream(fullFile, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+                stream.Close();
+
+                if (fullFile.ToLower().EndsWith(".jpg"))
+                {
+                    ProcessImage(filePath, file.FileName);
+                }
+            }
+
+            return Ok(new { file = "/media/" + file.FileName, thumbnail = "/media/" + "th-" + file.FileName });
+        }
+
+        public void ProcessImage(string dir, string file)
+        {
+            Image<Rgba32> image = Image.Load(dir + file);
+
+            image.Mutate(x => x
+            .Resize(0, 1080)
+            );
+
+            image.Save(dir + file);
+
+            image.Mutate(x => x
+            .Resize(0, 300)
+            );
+
+            image.Save(dir + "th-" + file);
+        }
+    }
+}
